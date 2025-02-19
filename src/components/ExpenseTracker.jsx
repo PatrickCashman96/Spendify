e856a58d4018f6eae7abf0899cd856f072845461
aoge2716 <aoge2716@gmail.com>
2025-02-20 00:47:26 +0100

    added barchart

--- src/components/ExpenseTracker.jsx
+++ src/components/ExpenseTracker.jsx
@@ -2,13 +2,15 @@ import ExpenseForm from "./ExpenseForm";
 import React, { useState, useEffect } from "react";
 import { db, auth } from "../firebase";
 import { collection, addDoc, query, where, onSnapshot, deleteDoc, doc } from "firebase/firestore";
-import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid } from "recharts";
+
 const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#ff6384", "#36a2eb"];
 
 const ExpenseTracker = () => {
   const [expenses, setExpenses] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null)
 
-
+  // get expense
   useEffect(() => {
     if (auth.currentUser) {
       const q = query(collection(db, "expenses"), where("userId", "==", auth.currentUser.uid));
@@ -19,25 +21,81 @@ const ExpenseTracker = () => {
     }
   }, []);
 
+  // remove an expense 
   const deleteExpense = async (id) => {
     await deleteDoc(doc(db, "expenses", id));
   };
 
+  // group data by category
   const groupedExpenses = expenses.reduce((acc,expense)=>{
     acc[expense.category] = (acc[expense.category]||0) + Number(expense.amount);
     return acc;
   },{});
   
-  console.log("GE",groupedExpenses)
+  // get piechart data
   const pieData = Object.keys(groupedExpenses).map((category, index)=>({
     name: category,
     value: groupedExpenses[category],
+  }));  
+  console.log("Pie Data: ", pieData);
+
+  // expense for selected category
+  const filteredExpenses = selectedCategory
+  ? expenses.filter(expense => expense.category === selectedCategory)
+  : [];
+
+  const sortedExpenses = [...filteredExpenses].sort((a,b)=> new Date(a.date)- new Date(b.date))
+
+  // get barchart data
+  const barData = filteredExpenses.map(expense => ({
+    name: expense.date,
+    description: expense.description,
+    value: Number(expense.amount),
   }))
-  
+  console.log("barData:",barData)
 
   return (
     <div>
       <h2>Expense Tracker</h2>
+      <h3> Expense by Category</h3>
+      <ResponsiveContainer width="100%" height={300} style={{ backgroundColor: "white" }}>
+        <PieChart>
+          <Pie 
+            data={pieData} 
+            dataKey="value" 
+            nameKey="name" 
+            cx="50%" 
+            cy="50%" 
+            outerRadius={100} 
+            label
+            onClick={(category,index)=>{
+              console.log("Clicked Category:", category.name);
+              setSelectedCategory(category.name);
+            }}
+          >
+            {pieData.map((entry,index)=> (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+            ))}
+          </Pie>
+          <Tooltip/>
+        </PieChart>
+      </ResponsiveContainer>
+      
+      {selectedCategory && (
+        <div>
+          <h3>{selectedCategory}</h3>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={barData}>
+            <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip formatter={(value, name, props) => [`$${value}`, `${props.payload.description}`]}/>
+              <Bar dataKey="value" fill="#8884d8" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      )}
+      
       <ExpenseForm/>
 
       <ul>
@@ -48,18 +106,6 @@ const ExpenseTracker = () => {
           </li>
         ))}
       </ul>
-      
-      <h3> Expense by Category</h3>
-      <ResponsiveContainer width="100%" height={300} style={{ backgroundColor: "white" }}>
-        <PieChart>
-          <Pie data={pieData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} label>
-            {pieData.map((entry,index)=> (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
-            ))}
-          </Pie>
-          <Tooltip/>
-        </PieChart>
-      </ResponsiveContainer>
     </div>
   );
 };
