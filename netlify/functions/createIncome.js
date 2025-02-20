fc24b70b0d9cc917b0c60f75edcb34942758d3c0
aoge2716 <aoge2716@gmail.com>
2025-02-20 11:16:25 +0100

    adding VITE to variable for netlify debugging

--- netlify/functions/createIncome.js
+++ netlify/functions/createIncome.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.VITE_FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -18,17 +18,19 @@ exports.handler = async (event) => {
     }
 
     const token = authHeader.split('Bearer ')[1];
+
     const decodedToken = await admin.auth().verifyIdToken(token);
     const userId = decodedToken.uid;
 
     // Interact with Firestore via UserID
     const incomeData = JSON.parse(event.body);
+
+
     incomeData.userId = userId;
     const newIncome = await db.collection('incomes').add(incomeData);
 
     return { statusCode: 201, body: JSON.stringify({ id: newIncome.id }) };
   } catch (error) {
-    console.error('Error creating income:', error);
-    return { statusCode: 500, body: JSON.stringify({ error: 'Failed to create income' }) };
+    return { statusCode: 500, body: JSON.stringify({ error: error.message , stack: error.stack }) };
   }
 };
