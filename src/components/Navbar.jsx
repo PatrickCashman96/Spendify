78d21b3ed8da81e1c09ff6b48b62726dc5b36b6a
aoge2716 <aoge2716@gmail.com>
2025-02-20 10:10:51 +0100

    edit title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar({toggleSidebar}){
     return(
         <div id="navbar">
             <button onClick={toggleSidebar}><img src="/assets/img/logo/burgermenu.png" alt="applogo" /></button>
-            <h1>webname name</h1>
+            <h1>Spendify</h1>
             <img src="/assets/img/logo/applogo.png" alt="" />
         </div>
     )
