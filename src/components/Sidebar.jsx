ad66d9d77bd81f11a90e620af0653798efc985eb
aoge2716 <aoge2716@gmail.com>
2025-02-20 02:45:29 +0100

    Sidebar for easier navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,20 @@
-export default function Sidebar(){
+import { Link } from "react-router-dom"
+import "./Sidebar.css"
+export default function Sidebar(props){
     return(
-        <div id="sidebar">
-            
+        <div id="sidebar" className={`${props.isOpen ? "open": ""}`}>
             <h1>Sidebar</h1>
-            
+            <Link to="/">
+                <h2>Home</h2>
+            </Link>
+
+            <Link to="/expenseTrack">
+                <h2>Expense</h2>
+            </Link>
+
+            <Link to="/incomeTrack">
+                <h2>Income</h2>
+            </Link>
         </div>
     )
 }
