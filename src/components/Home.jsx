8ec06c39c5cd78be357f115524ec3cde69d2d6db
aoge2716 <aoge2716@gmail.com>
2025-02-20 15:25:09 +0100

    removed extra code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,37 +3,12 @@ import { useEffect, useState } from "react"
 
 export default function Home(){
     
-    const [incomes, setIncomes] = useState([]);
-
     
-    useEffect(()=>{
-        const fetchIncomes = async () => {
-            try{
-                const response = await fetch("/.netlify/functions/getIncome");
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-
-                const data = await response.json();
-                setIncomes(data);
-            } catch (error){
-                console.error("error fetch incomes: ", error);
-            }            
-        } 
-        fetchIncomes();
-    }, [])
 
     return(
         <div className="home">
             
             <h1>Home</h1>
-            
-            {incomes.map(el=>(
-                <div key={index}>
-                    {console.log(el)} 
-                    <p>{el.id}: {JSON.stringify(el)}</p>
-                </div>
-            ))}
             
             
             
