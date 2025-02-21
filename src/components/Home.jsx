f77d52f4930c31e804cb86dd8ebef87a36df8a10
aoge2716 <aoge2716@gmail.com>
2025-02-21 03:57:42 +0100

    display income and expense

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
 import { auth } from "../firebase";
 import { Link } from "react-router-dom";
-
-export default function Home({incomes, expenses}){
+import ExpenseChart from "./ExpenseChart";
+import IncomeChart from "./IncomeChart";
+export default function Home({incomes, expenses, setExpenses, setIncomes}){
     console.log(incomes)
     const totalIncome = incomes.reduce((acc, income)=> acc+Number(income.amount), 0);
     console.log("totalIncome: ", totalIncome)
@@ -14,7 +15,20 @@ export default function Home({incomes, expenses}){
             
             {/* <h1>Hello {auth.currentUser.email}</h1> */}
             <h2>You currently have ${currentSaving} in your saving</h2>
+            <section>
+                <h2>Expenses</h2>
+                <ExpenseChart expenses={expenses} setExpenses={setExpenses}/>
+            </section>
+            
+            <section>
+                <h2>Incomes</h2>
+                <IncomeChart incomes={incomes} setIncomes={setIncomes}/>
+            </section>
+            
+
             <h3>Go to <Link to="/incomeTrack">Income</Link> or <Link to="/expenseTrack">Expense</Link> for further detail</h3>
+
+            
             
         </div>
     )
